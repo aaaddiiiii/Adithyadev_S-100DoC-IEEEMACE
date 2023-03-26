43a764e4127e4db41fd268b307bfc1240870d149
aaaddiiiii <118617390+aaaddiiiii@users.noreply.github.com>
2023-03-26 13:08:14 +0530

    Update WEEK 1 Q3.py

--- WEEK 1 Q3.py
+++ WEEK 1 Q3.py
@@ -1,12 +1,9 @@
-list1=[1,2,3]
-print("\n",list1)
-while True:
-    num=int(input("Enter the number you want to enter to the list : "))
-    list1.append(num)
-    print("New List : ",list1)
-    choice=input("\nDo you want to continue (0=FALSE 1=TRUE) : ")
-    if choice=="0":
-        print("\nThe final list is : ",list1)
-        break
+list=[1,2,3]
+nums=input("\nEnter the numbers separated by commas : ")
+for i in nums:
+    if i.isdigit():
+        number=int(i)
+        list.append(number)
     else:
-        print("")
+        continue
+print(list)
