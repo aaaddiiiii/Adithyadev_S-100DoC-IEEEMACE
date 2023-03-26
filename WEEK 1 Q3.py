fed786dc8c0d767a39904ad4a466ddebd35d5150
aaaddiiiii <118617390+aaaddiiiii@users.noreply.github.com>
2023-03-26 17:08:15 +0530

    Update WEEK 1 Q3.py

--- WEEK 1 Q3.py
+++ WEEK 1 Q3.py
@@ -1,9 +1,12 @@
-list=[1,2,3]
-nums=input("\nEnter the numbers separated by commas : ")
-for i in nums:
-    if i.isdigit():
-        number=int(i)
-        list.append(number)
-    else:
-        continue
-print(list)
+list1=[1,2,3]
+nums=eval(input("Enter the numbers separated by commas : "))
+if type(nums)==int:
+    list1.append(nums)
+else:
+    for i in nums:
+        if str(i).isdigit():
+            number=int(i)
+            list1.append(number)
+        else:
+            continue
+print(list1)
